f2abac5f3d6e9bdfcd6a0a21994905d7fc7cc6bc
pechono <balsamo2@hotmail.com>
2022-03-04 15:01:38 -0300

    creando consulta de busqueda

--- cierrecaja_Anterio.php
+++ cierrecaja_Anterio.php
@@ -1,7 +1,33 @@
 <?php
 include_once "funciones.php";
 include_once "encabezado.php";
+
+if(!iseet($_POST["usuario"]) || $_POST["usuario"]==""){
+
+$usuarioPost="cierrecaja.id_usuario like '%'";
+}else{
+    $usuarioPost="cierrecaja.id_usuario"=.$_POST["usuario"];
+
+}
+
+if(!iseet($_POST["fecha"]) || $_POST["fecha"]==""){
+
+  $fechaPost="cierrecaja.fecha like '%'";
+    }else{
+        $fechaPost="cierrecaja.fecha".$_POST["fecha"];
+    }
+    
+    if(!iseet($_POST["turno"]) || $_POST["turno"]==""){
+
+        $turnoPost="cierrecaja.turno like '%'";
+        }else{
+            $turnoPost="cierrecaja.turno".$_POST["usuario"];
+        }
+        
 ?>
+
+
+
 
 <div class="container">
     <form action="cierrecaja_Anterio.php" metohod="post">
@@ -14,10 +40,11 @@ include_once "encabezado.php";
     <tr>
         <td><input type="date" name="fecha"></td>
         <td><select name="usuario">
+            <option value="">Selecionar Usuario</option>
             <?php 
              $Usuarios=usuario();
             foreach($Usuarios as $User){
-                echo "<option value='". $User->id_usuario .">". $User->apellido.", ".$User->nombre."</option>";
+                echo "<option value='". $User->id_usuario ."'>". $User->apellido.", ".$User->nombre."</option>";
             }?>
 
         </select></td>
